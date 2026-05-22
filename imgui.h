c4949a63d88a4e5016bb9f940321186ff7f5a4d4
XS AI <xsai0528@gmail.com>
2026-05-22 19:34:31 +0800

    Update imgui.h

--- imgui.h
+++ imgui.h
@@ -1,17 +1,16 @@
 #pragma once
+
 namespace ImGui {
-    struct ImGuiContext;
+    struct ImGuiContext {};
+    struct ImDrawData {};
+
     ImGuiContext* CreateContext(void* shared_font_atlas = nullptr);
     void DestroyContext(ImGuiContext* ctx = nullptr);
-    struct ImGuiIO& GetIO();
     void NewFrame();
     void Render();
-    struct ImDrawData* GetDrawData();
+    ImDrawData* GetDrawData();
     bool Begin(const char* name, bool* p_open = nullptr, int flags = 0);
     void End();
     void Text(const char* fmt, ...);
     bool Button(const char* label, const float& size = 0.0f);
 }
-struct ImGuiIO {
-    bool WantCaptureMouse;
-};
