51edcf692ba90237d33291b0e723a981c5376d37
XS AI <xsai0528@gmail.com>
2026-05-22 19:37:41 +0800

    Update imgui.cpp

--- imgui.cpp
+++ imgui.cpp
@@ -1,16 +1,2 @@
 #include "imgui.h"
-
-namespace ImGui {
-    ImGuiContext* CreateContext(void* shared_font_atlas) { 
-        static ImGuiContext dummy_ctx;
-        return &dummy_ctx; 
-    }
-    void DestroyContext(ImGuiContext* ctx) {}
-    void NewFrame() {}
-    void Render() {}
-    ImDrawData* GetDrawData() { return nullptr; }
-    bool Begin(const char* name, bool* p_open, int flags) { return true; }
-    void End() {}
-    void Text(const char* fmt, ...) {}
-    bool Button(const char* label, const float& size) { return true; }
-}
+// 暫時留空，僅作為編譯目標
